c6ead8d71dbf98bdbe6e5d5b82263d084b39890b
N0THINKK <149356793+N0THINKK@users.noreply.github.com>
2026-01-19 11:48:33 +0700

    refactor(shared): extract reusable UI from group_leader

--- shared/presentation/components/AppCard.cs
+++ shared/presentation/components/AppCard.cs
@@ -1,50 +1,89 @@
+using System;
 using System.ComponentModel;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
-using mtc_app.shared.presentation.styles;
+using mtc_app.shared.presentation.utils;
 
 namespace mtc_app.shared.presentation.components
 {
     public class AppCard : Panel
     {
+        private int _cornerRadius = 8;
+        private bool _showShadow = false;
+        private Color _borderColor = Color.FromArgb(230, 230, 230);
+
+        [Category("Appearance")]
+        public int CornerRadius
+        {
+            get => _cornerRadius;
+            set { _cornerRadius = value; Invalidate(); }
+        }
+
+        [Category("Appearance")]
+        public bool ShowShadow
+        {
+            get => _showShadow;
+            set { _showShadow = value; Invalidate(); }
+        }
+
+        [Category("Appearance")]
+        public Color BorderColor
+        {
+            get => _borderColor;
+            set { _borderColor = value; Invalidate(); }
+        }
+
         public AppCard()
         {
-            this.BackColor = AppColors.Surface;
-            this.Padding = new Padding(AppDimens.SpacingMedium);
+            this.SetStyle(ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.ResizeRedraw, true);
+            this.BackColor = Color.White;
+            this.Padding = new Padding(10);
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            base.OnPaint(e);
-
-            // Draw shadow or border
-            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            // Do NOT call base.OnPaint if you want full control, but for Panel it's fine.
+            // We want to draw the background ourselves to handle transparency/corners
             
-            int radius = AppDimens.CornerRadiusLarge;
-            Rectangle rectangle = this.ClientRectangle;
-            rectangle.Width -= 1;
-            rectangle.Height -= 1;
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
 
-            using (var path = GetRoundedPath(rectangle, radius))
-            using (var pen = new Pen(AppColors.Border, 1))
+            // Define bounds
+            Rectangle bounds = new Rectangle(0, 0, this.Width - 1, this.Height - 1);
+            if (_showShadow)
             {
-                e.Graphics.DrawPath(pen, path);
+                // Shrink bounds slightly to make room for shadow
+                bounds.Inflate(-2, -2);
             }
-        }
 
-        private GraphicsPath GetRoundedPath(Rectangle rectangle, int radius)
-        {
-            GraphicsPath path = new GraphicsPath();
-            int d = radius * 2;
+            // Draw Shadow (Simple implementation)
+            if (_showShadow)
+            {
+                // In WinForms GDI+, real shadows are hard. We simulate with a gray offset.
+                using (GraphicsPath shadowPath = GraphicsUtils.GetRoundedRectangle(new Rectangle(bounds.X + 2, bounds.Y + 2, bounds.Width, bounds.Height), _cornerRadius))
+                {
+                    using (SolidBrush shadowBrush = new SolidBrush(Color.FromArgb(20, 0, 0, 0)))
+                    {
+                        g.FillPath(shadowBrush, shadowPath);
+                    }
+                }
+            }
 
-            path.AddArc(rectangle.X, rectangle.Y, d, d, 180, 90);
-            path.AddArc(rectangle.X + rectangle.Width - d, rectangle.Y, d, d, 270, 90);
-            path.AddArc(rectangle.X + rectangle.Width - d, rectangle.Y + rectangle.Height - d, d, d, 0, 90);
-            path.AddArc(rectangle.X, rectangle.Y + rectangle.Height - d, d, d, 90, 90);
-            path.CloseFigure();
+            // Draw Background
+            using (GraphicsPath path = GraphicsUtils.GetRoundedRectangle(bounds, _cornerRadius))
+            {
+                using (SolidBrush brush = new SolidBrush(this.BackColor))
+                {
+                    g.FillPath(brush, path);
+                }
 
-            return path;
+                // Draw Border
+                using (Pen pen = new Pen(_borderColor))
+                {
+                    g.DrawPath(pen, path);
+                }
+            }
         }
     }
 }
