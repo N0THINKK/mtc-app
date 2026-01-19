d70bc48a5f331536548bd23b6c7c373be327ef16
N0THINKK <149356793+N0THINKK@users.noreply.github.com>
2026-01-19 11:29:53 +0700

    refactor(shared): extract reusable UI and data layer from authentication

---
+++ shared/data/dtos/UserDto.cs
@@ -0,0 +1,11 @@
+namespace mtc_app.shared.data.dtos
+{
+    public class UserDto
+    {
+        public long UserId { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
